8c527626684d705d8fa00a75bda2f178a5709768
dshepsis <dfshepsis@gmail.com>
2022-03-29 22:08:18 -0400

    Just a filler implementation of /misc

--- commands/misc.mjs
+++ commands/misc.mjs
@@ -1,4 +1,19 @@
 // For miscellaneous commands which are mostly just for fun and don't serve a
 // real purpose :)
 
-// Subcommands: BORK, autoride
+// Subcommands: BORK, auride-says, etc.?
+
+import { SlashCommandBuilder } from '@discordjs/builders';
+
+export const data = (new SlashCommandBuilder()
+	.setName('misc')
+	.setDescription('Miscellaneous commands which are mostly just for fun.')
+);
+
+// This command will only be visible and usable from the development guild
+export const inDevelopment = true;
+
+export async function execute(interaction) {
+	const content = 'BORK!';
+	return interaction.reply({ content });
+}
