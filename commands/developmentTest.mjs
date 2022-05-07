feb35ee76c07db41fd184416d985ba71a8763852
dshepsis <dfshepsis@gmail.com>
2022-05-07 13:10:40 -0400

    Revert "Revert "Revert "Revert "A ton of changes for Twitch Stream monitoring WIP""""
    This reverts commit dc520a0d6646c8cb93a254ad9d84411a61b68f9f.

--- commands/developmentTest.mjs
+++ commands/developmentTest.mjs
@@ -2,13 +2,17 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 
 export const data = (new SlashCommandBuilder()
 	.setName('test')
-	.setDescription('Test whether this in-development command is available only in the dev server.')
+	.setDescription('Crashes the bot to test pm2\'s automatic restart.')
+	// .addStringOption(option => option
+	// 	.setName('message-id')
+	// 	.setDescription('The ID of the message you\'d like to delete')
+	// 	.setRequired(true)
+	// )
 );
 
 // This command will only be visible and usable from the development guild
 export const inDevelopment = true;
 
-export async function execute(interaction) {
-	const content = 'Hacked :D';
-	return await interaction.reply({ content });
+export async function execute() {
+	throw new Error('Intentionally crashing the bot...');
 }
--- events/ready.mjs
+++ events/ready.mjs
@@ -1,12 +1,12 @@
 export const name = 'ready';
 export const once = true;
 export async function execute(client) {
-	console.log(`Logged in as ${client.user.tag} at ${Date()}. Setting up commands...`);
+	console.log(`Client ready as ${client.user.tag}. Setting up commands...`);
 
 	(Array.from(client.commands.values())
 		.filter(command => typeof command.onClientReady === 'function')
 		.map(command => command.onClientReady(client))
 	);
 
-	console.log(`Finished setting up commands at ${Date()}!`);
+	console.log('Finished setting up commands!');
 }
