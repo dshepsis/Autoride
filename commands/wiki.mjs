973920669b3a313228e7afe22538aa9c599f4d2f
auride <themuffinelf@gmail.com>
2024-02-17 23:18:29 -0500

    use wiki config.json properties in /wiki command

--- commands/wiki.mjs
+++ commands/wiki.mjs
@@ -1,7 +1,10 @@
 import { fetchJSON } from './command-util/fetchJSON.mjs';
 import { SlashCommandBuilder, escapeMarkdown } from 'discord.js';
 
-const WIKI_API_BASE = 'https://okami.speedruns.wiki/api.php?';
+import { pkgRelPath } from '../util/pkgRelPath.mjs';
+import { importJSON } from '../util/importJSON.mjs';
+const { wiki } = await importJSON(pkgRelPath('./config.json'));
+
 export const data = (new SlashCommandBuilder()
 	.setName('wiki')
 	.setDescription('Get a link to a page on the Okami Speedrunning Wiki.')
@@ -29,7 +32,7 @@ export async function execute(interaction) {
 	const sanitizedQueryComponent = encodeURIComponent(query).replaceAll('<', '');
 
 	// Use the MediaWiki API to search page titles for the best-matching page:
-	const queryURL = `${WIKI_API_BASE}action=query&list=search&srsearch=${sanitizedQueryComponent}&srlimit=1&format=json`;
+	const queryURL = `${wiki.apiURL}action=query&list=search&srsearch=${sanitizedQueryComponent}&srlimit=1&srprop=wordcount&format=json`;
 	let response;
 	try {
 		response = (await fetchJSON(queryURL)).query.search[0];
@@ -51,7 +54,7 @@ export async function execute(interaction) {
 	// This avoids having to make an extra request to check for sure:
 	if (response.wordcount <= 20) {
 		// Check if the page is a redirect, and automatically resolve it if so:
-		const redirectCheckURL = `${WIKI_API_BASE}action=query&pageids=${response.pageid}&redirects&format=json`;
+		const redirectCheckURL = `${wiki.apiURL}action=query&pageids=${response.pageid}&redirects&format=json`;
 		const redirects = (await fetchJSON(redirectCheckURL)).query.redirects;
 		if (redirects !== undefined) {
 			resolvedTitle = redirects[0].to;
@@ -60,6 +63,6 @@ export async function execute(interaction) {
 	// MediaWiki automatically converts spaces in the URL to underscores anyways,
 	// so we do that ourselves to make the URL cleaner:
 	const pagePath = resolvedTitle.replace(/\s/g, '_');
-	const content = `https://okami.speedruns.wiki/${pagePath}`;
+	const content = wiki.URL + pagePath;
 	return await interaction.reply({ content });
 }
